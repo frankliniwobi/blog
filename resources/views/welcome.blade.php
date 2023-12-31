5cb451e7a029da68ce57712d78b42ea29bda7386
Iwobi Okwudili Frank <iwobiokwudili@gmail.com>
2023-12-31 10:29:19 +0100

    arrange the layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,11 +73,11 @@
 
             @if ($posts->count() > 1)
 
-                <div class="lg:grid lg:grid-cols-2">
+                <div class="lg:grid lg:grid-cols-6">
 
                     @foreach ($posts->skip(1) as $post)
 
-                        <x-blog-card />
+                        <x-blog-card :post="$post" class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}" />
 
                     @endforeach
 
